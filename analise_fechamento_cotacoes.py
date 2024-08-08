720733aab6bf846013a54eab2b7d23f9a58eacea
Débora  Santos <deboramargaretedossantos@gmail.com>
2024-08-08 18:22:25 -0300

    envio_automatizado_e-mail

--- analise_fechamento_cotacoes.py
+++ analise_fechamento_cotacoes.py
@@ -29,3 +29,69 @@ print(maxima)
 print(min)
 print(media)
 
+# Definido tempo para abrir a caixa de e-mail;
+
+time.sleep(5)
+print(pyautogui.position)
+
+# Criando variáveis para destinatário e assunto;
+
+destinatário = "deboramargaretedossantos@gmail.com"
+assunto = "Análises Cotação Ação"
+
+# Criando mensagem padrão para corpo do e-mail;
+
+mensagem = f"""
+
+Bom dia!
+
+    Seguem análises da ação {ticker} do período de {dt_inicial} a {dt_final}:
+
+    Cotação máxima: R$ {maxima};
+    Cotação mínima: R$ {minima};
+    Valor médio: R$ {media}.
+
+    Atenciosamente, 
+
+    Maria Salgado
+    Analista
+"""
+# Configurar uma pausa entre as ações do pyautogui;
+
+pyautogui.PAUSE = 3
+
+# Abrir o navegador no gmail
+
+webbrowser.open("deboramargaretedossantos@gmail.com")
+sleep(3)
+
+# Clicar no botão "Escrever"
+
+pyautogui.click(x=2503, y=314)
+
+# Preencher Para
+
+pyperclip.copy(destinatário)
+pyautogui.hotkey("ctrl", "v")
+pyautogui.hotkey("tab")
+
+# Preencher Assunto
+
+pyperclip.copy(assunto)
+pyautogui.hotkey("ctrl" , "v")
+pyautogui.hotkey("tab")
+
+# Preencher corpo do e-mail
+
+pyperclip.copy(mensagem)
+pyautogui.hotkey("ctrl" , "v")
+
+# Clicar no botão enviar
+
+pyautogui.click(x=4056, y=1360)
+
+# fechar a aba
+
+pyautogui.hotkey("ctrl" , "f4")
+print("E-mail enviado com sucesso!")
+
