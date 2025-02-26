62d4ef4f65d2d1ae17bd6ba05b659604191aa25f
DeboraSantos <deboramargaretedossantos@gmail.com>
2025-02-26 18:04:15 -0300

    automatizando_cadastro_produto_linha_a_linha

--- automacao.py
+++ automacao.py
@@ -27,11 +27,31 @@ pyautogui.click(x=596, y=377)
 pyautogui.write("deborasantos@gmail.com")
 pyautogui.press("enter")
 pyautogui.click(x=546, y=470)
-pyautogui.write("1234")
+pyautogui.write("****")
 pyautogui.press("enter")
 
 #Aqui precisamos percorrer as linhas da tabela
 #Para cada linha vamos cadastrar um produto
 for linha in tabela.index:
-    pyautogui.click()
-
+    #Clica no primeiro campo
+    pyautogui.click(x=470, y=254)
+    #Pega o código da tabela e escreve no campo
+    pyautogui.write(str(tabela.loc[linha, "codigo"]))
+    #passando para o próximo campo
+    pyautogui.press("enter")
+    #segue essa sequência para os demais campos
+    pyautogui.write(str(tabela.loc[linha, "marca"]))
+    pyautogui.press("enter")
+    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.press("enter")
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press("enter")
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.press("enter")
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press("enter")
+    pyautogui.write(str(tabela.loc[linha, "obs"]))
+    pyautogui.press("enter")
+    #verifica se tem informação em "obs", caso contrário não preenche
+    if not pd.isna(tabela.loc[linha, "obs"]):
+        pyautogui.write(str(tabela.loc[linha, "obs"]))
