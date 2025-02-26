6b0f9f243fc7567310a57b477d4e8e087ba3f617
DeboraSantos <deboramargaretedossantos@gmail.com>
2025-02-26 16:53:43 -0300

    correcao_importacao_bibliotecas

--- auxiliar.py
+++
@@ -1,6 +0,0 @@
-# Biblioteca pyautogui usada aqui com a finalidade de controlar o mouse e o teclado, realizar capturas de tela, clicar em campos, digitar textos, entre outros
-import pyautogui
-# Biblioteca pandas usada aqui Leitura e escrita de arquivos como CSV
-import pandas as pd
-# Pausar a execução de um programa por um tempo específico com
-import time
