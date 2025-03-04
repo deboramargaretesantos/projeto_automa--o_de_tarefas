081075b5e550e846b6ad31e4578f348178cd44f1
DeboraSantos <deboramargaretedossantos@gmail.com>
2025-03-04 17:11:42 -0300

    inclusoes _e_alteracoess_em_comentarios

--- automacao.py
+++ automacao.py
@@ -2,34 +2,32 @@
 import pyautogui
 #Biblioteca pandas usada aqui Leitura e escrita de arquivos como CSV
 import pandas as pd
-#Pausar a execução de um programa por um tempo específico com
+#Pausa a execução de um programa por um tempo específico com
 import time
 
 #Importando base de dados
 tabela = pd.read_csv("produtos2.csv")
 print(tabela)
 
-#define o tempo de espera entre os comandos do pyautogui
-pyautogui.PAUSE = 1.0
+#Tempo de espera entre os comandos do Pyautogui.
+pyautogui.PAUSE = 1.5
 
-#Abrindo o sitema usando chrome, usando pyautogui
+#Abre o sitema usando chrome, usando pyautogui
 pyautogui.press("win")
 pyautogui.write("chome")
 pyautogui.press("enter")
 link = ("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 pyautogui.write(link)
 pyautogui.press("enter")
+#Tempo de espera para abertura do sistema, antes que a próxima etapa comece.
 
-#tempo de espera para abertura do sistema
-time.sleep(2)
-#fazendo login (qualquer e-mail e senha funcionará)
+#Faz o  login (qualquer e-mail e senha funcionará)252.95
 pyautogui.click(x=596, y=377)
 pyautogui.write("deborasantos@gmail.com")
 pyautogui.press("enter")
 pyautogui.click(x=546, y=470)
 pyautogui.write("****")
 pyautogui.press("enter")
-
 
 #Aqui precisamos percorrer as linhas da tabela
 #Para cada linha vamos cadastrar um produto
@@ -38,11 +36,11 @@ for linha in tabela.index:
     pyautogui.click(x=381, y=255)
     pyautogui.hotkey('ctrl', 'a')  # Seleciona todo o texto
     pyautogui.press('delete')  # Deleta o texto selecionado
-    #Pega o Under Armourcódigo da tabela e escreve no campo
+    #Busca o código da tabela e escreve no campo
     pyautogui.write(str(tabela.loc[linha, "codigo"]))
-    #passando para o próximo campo
+    #Passa para o próximo campo
     pyautogui.press("tab")
-    #segue essa sequência para os demais campos
+    #Segue essa sequência para os demais campos
     pyautogui.write(str(tabela.loc[linha, "marca"]))
     pyautogui.press("tab")
     pyautogui.write(str(tabela.loc[linha, "tipo"]))
@@ -55,7 +53,7 @@ for linha in tabela.index:
     pyautogui.press("tab")
     pyautogui.write(str(tabela.loc[linha, "obs"]))
     pyautogui.press("tab")
-    #verifica se tem informação em "obs", caso contrário não preenche
+    #Verifica se tem informação em "obs", caso contrário não preenche
     if not pd.notna(tabela.loc[linha, "obs"]):
        pyautogui.write(str(tabela.loc[linha, "obs"]))
 
--- auxiliar.py
+++ auxiliar.py
@@ -1,7 +1,7 @@
 import pyautogui
 import time
 
-time.sleep(5)
+time.sleep(0.5)
 #Pegar posição do mouse para saber onde clicar 
 print(pyautogui.position())
 
